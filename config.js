4eaf0567908b155b22c06faff08a8386a605b48c
hesh1111 <156193054+hesh1111@users.noreply.github.com>
2024-12-29 19:40:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,11 +8,11 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "bi4VyZLK#SLB869c50IjrtGC3-zcvbq9aZW6dNTozqC3l9lhuMqU", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-AUTO_REACT: process.env.AUTO_REACT || "true",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
+AUTO_REACT: process.env.AUTO_REACT || "false",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",    
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",    
 };
